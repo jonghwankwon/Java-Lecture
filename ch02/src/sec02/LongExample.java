123000b08b5608805a6e0c0f5a0be9e9bc49f53c
jonghwan kwon <whdghks1048@naver.com>
2019-03-14 13:18:48 +0900

    first commit

---
+++ ch02/src/sec02/LongExample.java
@@ -0,0 +1,17 @@
+package sec02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long var1 =  10L; //반드시 L을 붙일것 소문자(l)은 1과 비슷해서 대문자 'L' 사용 권장
+		long var2 =  20L;
+		//long var3 =  1000000000000; //컴파일 에러
+		long var4 =  1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
