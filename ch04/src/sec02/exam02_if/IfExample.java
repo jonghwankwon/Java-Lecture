26ee5294a5d128de7114ce7819f469d3caf9f724
jonghwan kwon <whdghks1048@naver.com>
2019-03-28 10:48:39 +0900

    ch04

---
+++ ch04/src/sec02/exam02_if/IfExample.java
@@ -0,0 +1,18 @@
+package sec02.exam02_if;
+
+public class IfExample {
+	public static void main(String[] args) {
+		int score = 93;
+		
+		if (score>=90) {
+			System.out.println("점수가 90보다 크거나 같습니다.");
+			System.out.println("등급은 A 입니다.");
+		}
+		
+		if (score < 90) {
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B 입니다.");
+		}
+		System.out.println("공통 사항입니다.");
+	}
+}
---
+++ ch04/src/sec02_01_If_Switch/ch04/src/sec03/exam01_for/EnhanceForExample.java
@@ -0,0 +1,16 @@
+package sec03.exam01_for;
+
+public class EnhanceForExample {
+	public static void main(String[] args) {
+		String[] names = {"강진희", "권오인", "권종환", "김성현", "노연아",
+							"백정호", "유상원", "은정우", "이아름", "정승아", 
+							"정정화", "천세은", "홍민지"};
+		
+		for (String name : names) {		// Enhanced for-loop
+			System.out.println(name);
+		}
+		for (int i=0; i<names.length; i++) {
+			System.out.println(names[i]);
+		}
+	}
+}
